4af9376fc89289a98e91bc6d416bbc623145541d
SoyCharita <mariangelesbaselli@hotmail.com>
2025-04-02 23:08:02 -0300

    Inicio del punto 2

--- src/main/java/punto2/Main.java
+++ src/main/java/punto2/Main.java
@@ -2,9 +2,9 @@ package punto2;
 
 public class Main {
     public static void main(String[] args) {
-        String palabra = "abcd";
+
         AprendiendoLambdas a = new AprendiendoLambdas();
-        a.unMetodo((String palabra) -> {
+        a.unMetodo((palabra) -> {
             if (0 == palabra.length() % 2) {
                 return true;
             }
